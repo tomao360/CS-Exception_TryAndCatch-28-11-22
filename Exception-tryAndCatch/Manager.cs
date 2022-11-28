268835eab2cdc16e1dbd007c28f0df0050845749
tomao360 <tomao360@gmail.com>
2022-11-28 13:17:56 +0200

    Exception-tryAndCatch

---
+++ Exception-tryAndCatch/Manager.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Exception_tryAndCatch
+{
+    internal class Manager
+    {
+        private Manager() { }
+
+        private readonly static Manager _instance = new Manager();
+        public static Manager INSTANCE
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        public void Ex01()
+        {
+            Console.WriteLine("Please enter 2 numbers: ");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int result;
+
+            try
+            {
+                result = a / b;
+                Console.WriteLine("The result is:{0}", result);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("{0}. The number is 0", ex.Message);
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void Ex02()
+        {
+            Console.WriteLine("Please enter a number:");
+            int num = int.Parse(Console.ReadLine());
+            int result;
+
+            try
+            {
+                result = 770 / num;
+                Console.WriteLine("The result is:{0}", result);
+            }
+            catch (DivideByZeroException ex)
+            {
+                while (num == 0)
+                {                  
+                    Console.WriteLine("Please enter another number:");
+                    num = int.Parse(Console.ReadLine());
+                }
+
+                result = 770 / num;
+                Console.WriteLine("The result is:{0}", result);
+            }
+
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void Ex03()
+        {
+            string[] str;
+
+            try
+            {
+                str = File.ReadAllLines(@"C:\Users\97252\source\repos\Exception-tryAndCatch\Exception-tryAndCatch\Ex02.txt");
+                for (int i = 0; i < str.Length; i++)
+                {
+                    Console.WriteLine(str[i]);
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void Read10Strings()
+        {
+            string[] str = new string[10];
+            int sumStr = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Please enter a string:");
+                str[i] = Console.ReadLine();
+                sumStr += str[i].Length;
+
+                if (sumStr > 100)
+                {
+                    throw new ArgumentException("You Entered strings with a total of more than 100 chrarcters");
+                }
+            }
+        }
+    }
+}
---
+++ Exception-tryAndCatch/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_tryAndCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Manager.INSTANCE.Ex01();
+            //Manager.INSTANCE.Ex02();
+            //Manager.INSTANCE.Ex03();
+
+            try
+            {
+                Manager.INSTANCE.Read10Strings();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+           
+
+            Console.ReadLine();
+        }
+    }
+}
